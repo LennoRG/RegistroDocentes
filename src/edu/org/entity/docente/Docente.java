4d15fcbed581a8abe257e87e14de07e61afa9678
LennoRG <lenorg24@gmail.com>
2019-09-08 21:22:40 -0500

    Version 01

---
+++ src/edu/org/entity/docente/Docente.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.org.entity.docente;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Docente {
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String genero;
+    private String edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+}
